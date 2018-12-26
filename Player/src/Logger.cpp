ed30c67b06a20e780b9f99eee70442afe2de09bf
Maximilian Kruse <maxkruse1996@gmail.com>
2018-12-26 17:51:40 +0100

    Log different levels per configuration

--- Player/src/Logger.cpp
+++ Player/src/Logger.cpp
@@ -11,6 +11,12 @@ namespace Parser {
 		spdlog::set_pattern("%^[%H:%M:%S:%e] %n: %v%$");
 
 		s_CoreLogger = spdlog::stdout_color_mt("DEBUG");
+#ifdef _DEBUG
 		s_CoreLogger->set_level(spdlog::level::trace);
+#elif _RELEASE
+		s_CoreLogger->set_level(spdlog::level::warn);
+#else
+		s_CoreLogger->set_level(spdlog::level::err);
+#endif
 	}
 }
--- Player/src/Logger.h
+++ Player/src/Logger.h
@@ -21,21 +21,9 @@ namespace Parser {
 
 }
 
-#ifdef _DEBUG
-
-// Client Logging Macros
+// Logging Macros
 #define LOGGER_TRACE(...)			::Parser::Logger::GetCoreLogger()->trace(__VA_ARGS__)
 #define LOGGER_INFO(...)			::Parser::Logger::GetCoreLogger()->info(__VA_ARGS__)
 #define LOGGER_WARN(...)			::Parser::Logger::GetCoreLogger()->warn(__VA_ARGS__)
 #define LOGGER_ERROR(...)			::Parser::Logger::GetCoreLogger()->error(__VA_ARGS__)
 #define LOGGER_CRITICAL(...)		::Parser::Logger::GetCoreLogger()->critical(__VA_ARGS__)
-
-#else
-
-#define LOGGER_TRACE(...)	
-#define LOGGER_INFO(...)	
-#define LOGGER_WARN(...)	
-#define LOGGER_ERROR(...)	
-#define LOGGER_CRITICAL(...)
-
-#endif
