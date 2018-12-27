ca58cb00bdaaab37eb85f83bc8767995d44354db
Maximilian Kruse <maxkruse1996@gmail.com>
2018-12-27 23:44:56 +0100

    Create Beatmap object based on inputs

--- Player/src/osu/Beatmap.cpp
+++ Player/src/osu/Beatmap.cpp
@@ -3,4 +3,23 @@
 
 namespace Parser {
 
+	Beatmap::Beatmap()
+	{
+		LOGGER_WARN("No Beatmap was parsed!");
+	}
+
+	Beatmap::Beatmap(
+		const std::string & DirectoryPath, std::vector<Hitobject> Hitobjects,
+		General& g, Metadata& m, SearchBy& s, Difficulty& d
+	)
+		: m_DirectoryPath(DirectoryPath), m_HitObjects(Hitobjects), m_General(g), m_Metadata(m), m_SearchBy(s), m_Difficulty(d)
+	{
+		LOGGER_INFO("Creating Beatmap \"{}\" with \n{}\n{}\n{}\n{}", DirectoryPath, m_General, m_Metadata, m_SearchBy, m_Difficulty);
+	}
+
+	Beatmap::~Beatmap()
+	{
+		LOGGER_INFO("Deleting Beatmap {}", m_DirectoryPath);
+	}
+
 }
