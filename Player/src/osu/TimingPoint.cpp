f984cb8003db601c9f85f8fe5a8eb4799dbb49b0
Maximilian Kruse <maxkruse1996@gmail.com>
2018-12-31 18:42:08 +0100

    Add TimingPoints

---
+++ Player/src/osu/TimingPoint.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "TimingPoint.h"
+
+namespace Parser
+{
+
+	TimingPoint::TimingPoint(long offset, double milliseconds_per_beat, unsigned short sampleset, unsigned short sampleindex, unsigned short volume, bool inherited)
+		: m_Offset(offset), m_MillisecondsPerBeat(milliseconds_per_beat), m_SampleSet(sampleset), m_SampleIndex(sampleindex), m_Volume(volume), m_Inherited(inherited)
+	{
+	}
+
+
+	TimingPoint::~TimingPoint()
+	{
+	}
+
+}
