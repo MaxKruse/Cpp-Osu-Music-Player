90f2413af9aa075651b1fb17e474537c6a509317
Maximilian Kruse <maxkruse1996@gmail.com>
2019-01-01 01:42:53 +0100

    Making Hitobject virtual

--- Player/src/osu/Hitobject.h
+++ Player/src/osu/Hitobject.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "pch.h"
 #include "Logger.h"
+#include "TimingPoint.h"
 
 namespace Parser {
 
@@ -18,18 +19,21 @@ namespace Parser {
 	class Hitobject
 	{
 	public:
-		Hitobject(unsigned short x, unsigned short y, long offset, unsigned short type); // TODO: x, y, time, type, >> hitSound..., extras <<
-		~Hitobject();
+		Hitobject(unsigned short x, unsigned short y, long offset, unsigned short type)
+			: m_Position(x, y), m_Offset(offset), m_Type(type)
+		{
+		}	// TODO: x, y, time, type, >> hitSound..., extras <<
+		~Hitobject()
+		{
+		}
 
+		virtual std::vector<std::string> GetHitsounds(TimingPoint t) { return std::vector<std::string>(); }
+		
 		inline const unsigned short GetX() const { return m_Position.X; }
 		inline const unsigned short GetY() const { return m_Position.Y; }
 		inline const long GetOffset() const { return m_Offset; }
 		inline const unsigned short GetType() const { return m_Type; }
-
-		// TODO: return a vector of vectors for all hitsounds for this hitobject, format example:
-		// std::map<long offset, std::vector<std::string> hitsounds>
-
-
+			   
 	protected:
 		Position       m_Position;
 		long           m_Offset;
