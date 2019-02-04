84c717c81f3b49eddce569ac5e07ebbcf211564a
Maximilian Kruse <maxkruse1996@gmail.com>
2019-02-04 15:24:54 +0100

    Logger Filename now Correct

--- Parser/src/Logger.cpp
+++ Parser/src/Logger.cpp
@@ -31,7 +31,7 @@ namespace Parser {
 		
 		const spdlog::filename_t &str(buffer);
 
-		s_FileLogger = spdlog::basic_logger_mt("FILELOG", "logs/basic-log.txt");
+		s_FileLogger = spdlog::basic_logger_mt("FILELOG", buffer);
 		s_FileLogger->set_level(spdlog::level::debug);
 		spdlog::flush_on(spdlog::level::warn);
 
